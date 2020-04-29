14f2bcba57b4cfbdd0720a28d8f3d1b4babd7b1d
rinadesigns <hello@rinakou.ch>
2020-04-29 10:37:39 +1000

    Initial commit

---
+++ care/index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<style>
+/* latin */
+@font-face {
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Montserrat Regliar'), local('Montserrat-Regliar'), url("../resources/Montserrat-Regliar.woff2") format('woff2');
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+}
+
+body {
+  background-color: black;
+  font-family: "Montserrat";
+  color: white;
+}
+
+#wrapper {
+  width: 500px;
+  margin: 0 auto;
+}
+
+#pageHeader {
+  margin: 0 auto;
+  width: 500px;
+}
+</style>
+</head>
+<body>
+<div id="wrapper">
+  <a href="../"><img id="pageHeader" src="../images/pageHeader.png"></img></a>
+  <h1>Care</h1>
+  A Warm Hug T-Shirts
+  <li>Machine wash cold with like colors</li>
+  <li>Do not bleach</li>
+  <li>Line dry in shade</li>
+  <li>Do not tumble dry</li>
+  <li>Iron inside out only</li>
+  <li>Do not dryclean</li>
+</div>
+</body>
+</html>
